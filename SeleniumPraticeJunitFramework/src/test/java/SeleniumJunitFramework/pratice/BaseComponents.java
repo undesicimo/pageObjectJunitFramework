30b115bc77fb7928a504d79cbe6b4502491884d2
unknown <jpz4373@linecorp.com>
2022-10-24 16:38:59 +0900

    インポート整理

--- SeleniumPraticeJunitFramework/src/test/java/SeleniumJunitFramework/pratice/BaseComponents.java
+++ SeleniumPraticeJunitFramework/src/test/java/SeleniumJunitFramework/pratice/BaseComponents.java
@@ -2,13 +2,7 @@ package SeleniumJunitFramework.pratice;
 
 
 import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
