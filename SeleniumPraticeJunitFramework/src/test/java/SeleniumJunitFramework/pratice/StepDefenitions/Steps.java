ee589bb9124e57e04c18bca74bd14ebb0cd636eb
undesicimo <a0ngel03@gmail.com>
2022-11-04 17:25:00 +0900

    cucumber featureファイル、Step definitions まで入れてみた

---
+++ SeleniumPraticeJunitFramework/src/test/java/SeleniumJunitFramework/pratice/StepDefenitions/Steps.java
@@ -0,0 +1,45 @@
+package SeleniumJunitFramework.pratice.StepDefenitions;
+
+import org.junit.Assert;
+
+import SeleniumJunitFramework.pratice.BaseComponents;
+import SeleniumJunitFramework.pratice.PageObjects.AdminPage;
+import SeleniumJunitFramework.pratice.PageObjects.LandingPage;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+
+
+public class Steps extends BaseComponents{
+    public LandingPage landingPage;
+    public AdminPage adminPage;
+    @Given("ブラウザを開く")
+    public void browser_open()
+    {
+        landingPage = launchBrowser();
+    }
+    @When("ログイン画面に遷移したあと")
+    public void after_going_to_login()
+    {
+        landingPage.goToPage();
+    }
+    @And("それぞれの画面上のメールとパスワードでログインする")
+    public void login_with_mailpass_onscreen()
+    {
+        landingPage.inputUsername(landingPage.getUsername().trim());
+        landingPage.inputPassword(landingPage.getPassword().trim());
+    }
+    @And("ログインボタンを押下")
+    public void press_login_button()
+    {
+        adminPage = landingPage.submitLogin();
+    }
+    @Then("ログインが成功し、admin画面が表示されて、{string} が表示される")
+    public void successful_login_check(String string)
+    {
+        Assert.assertEquals(string, adminPage.getHeaderTitle());
+        System.out.println(adminPage.getHeaderTitle());
+    }
+}
